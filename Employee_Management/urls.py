a59167816e42f61a9c68b408acc1958e24cfbf2d
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-26 23:53:53 +0600

    added a class that show a user info directly in the admin pannel

--- Employee_Management/urls.py
+++ Employee_Management/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+admin.site.site_header = "Abdullah Admin"
+admin.site.site_title = "Employee Management System"
+admin.site.index_title = "Welcome to Employee Management System"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('app1.urls')),
--- app1/admin.py
+++ app1/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from  .models import Employee
 # Register your models here.
-admin.site.register(Employee)
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['name','email','password']
+
+admin.site.register(Employee,EmployeeAdmin)
