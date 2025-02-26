379f1be4def89f473893bb2251b1e4b12b63b70a
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-26 23:29:32 +0600

    added html files and map them with their coresponding functions

--- app1/urls.py
+++ app1/urls.py
@@ -3,4 +3,5 @@ from django.urls import path,include
 from app1 import views
 urlpatterns = [
     path('',views.HomePage,name='home'),
+    path('update/',views.Update,name='update'),
 ]
--- app1/views.py
+++ app1/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render,HttpResponse,redirect
 
 # Create your views here.
 def HomePage(request):
-    return HttpResponse("Hi")
+    return render(request,'app1/index.html')
+
+def Update(request):
+    return render(request,'app1/update.html')
