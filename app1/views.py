a4ce8fae67d3aff1e89ef7ccc2b903fb86318541
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-27 00:06:12 +0600

    added form

--- app1/views.py
+++ app1/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,HttpResponse,redirect
-
+from .forms import EmployeForm
 # Create your views here.
 def HomePage(request):
-    return render(request,'app1/index.html')
+    form = EmployeForm()
+    return render(request,'app1/index.html',{"form":form})
 
 def Update(request):
     return render(request,'app1/update.html')
